9d218a1a880bf31b820044842538bdc74032d9a0
Gokul <gokulr488@gmail.com>
2021-06-02 11:56:10 +0530

    update triggers working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,27 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  console.log("Test Log");
-  response.send("Fleezy says Hi");
-});
+admin.initializeApp();
+
+export const expenseDocUpdated = functions.firestore
+  .document("Companies/{companyID}/Expense/{expenseDocID}")
+  .onUpdate((change, context) => {
+    const expenseDoc = change.after.data;
+    if (expenseDoc != null) {
+      console.log(expenseDoc);
+      console.log(context.params.expenseDocID);
+    }
+  });
+
+export const tripDocUpdated = functions.firestore
+  .document("Companies/{companyID}/Trip/{tripDocID}")
+  .onUpdate((change, context) => {
+    const tripDoc = change.after.data;
+    if (tripDoc != null) {
+      console.log(tripDoc);
+      console.log(context.params.tripDocID);
+    }
+  });
